ab986c4ae0a97ab3d479718fed2dd0a86396c26a
Krit Cholapand <flap.atyz@gmail.com>
2021-03-14 06:36:22 +0700

    :sparkles: Init Unity Project

---
+++ Assets/Scripts/PlayerMovementController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovementController : MonoBehaviour
+{
+    [SerializeField] private Rigidbody2D rigidbody;
+
+    [SerializeField] private float xSpeed = 2.0f;
+    [SerializeField] private float ySpeed = 2.0f;
+    private int inputX, inputY;
+
+
+    private void Awake()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        var rawInputX = Input.GetAxisRaw("Horizontal");
+        var rawInputY = Input.GetAxisRaw("Vertical");
+        inputX = rawInputX == 0 ? 0 : (int)Mathf.Sign(rawInputX);
+        inputY = rawInputY == 0 ? 0 : (int)Mathf.Sign(rawInputY);
+    }
+
+    private void FixedUpdate()
+    {
+        rigidbody.velocity = new Vector2(inputX * xSpeed, inputY * ySpeed);
+    }
+}
