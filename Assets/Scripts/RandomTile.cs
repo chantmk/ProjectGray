9271db0b19ec3db2238e5e4fcd9b3d7aeb2652cc
Pongsiri Ngamboontham <p_ngamboontham@hotmail.com>
2021-03-16 17:38:44 +0700

    :sparkles: dangerTile color change  only player's position

--- Assets/Scripts/RandomTile.cs
+++ Assets/Scripts/RandomTile.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
@@ -8,54 +9,75 @@ public class RandomTile : MonoBehaviour
 {
     public Tilemap tilemap;
     private List<Vector3> tileWorldLocations;
-    private int[,] dangerTile = new int[12, 6] { { 1, 0, 1, 0, 0, 1 }, { 1, 0, 1, 0, 0, 1 }, { 1, 0, 1, 0, 0, 1 }, { 1, 0, 1, 0, 0, 1 }, { 1, 0, 1, 0, 0, 1 }, { 1, 0, 1, 0, 0, 1 }, { 1, 0, 1, 0, 0, 1 }, { 1, 0, 1, 0, 0, 1 }, { 1, 0, 1, 0, 0, 1 }, { 1, 0, 1, 0, 0, 1 }, { 1, 0, 1, 0, 0, 1 }, { 1, 0, 1, 0, 0, 1 }, };
+    private float[,] dangerTile = new float[12, 6] { { 0, 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0, 0}, { 0, 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0, 0 }, };
     // Start is called before the first frame update
     BoundsInt bounds;
-    
-    
+    private GameObject playerObj = null;
+    int objX;
+    int objY;
 
     void Start()
     {
         tilemap = GetComponent<Tilemap>();
         bounds = tilemap.cellBounds;
         TileBase[] allTiles = tilemap.GetTilesBlock(bounds);
-
-        
+        if (playerObj == null)
+            playerObj = GameObject.FindGameObjectWithTag("Player");
         EventPublisher.PlayerFire += processArrayColor;
-        
-    }
-
-    void processArrayColor()
-    {
         for (int x = bounds.xMin + 4; x < bounds.xMax - 1; x++)
         {
             for (int y = bounds.yMin; y < bounds.yMax - 4; y++)
             {
-                /*TileBase tile = allTiles[x + y * bounds.size.x];
-                if (tile != null)
-                {
-                    Debug.Log("x:" + x + " y:" + y + " tile:" + tile.name);
-                }
-                else
-                {
-                    Debug.Log("x:" + x + " y:" + y + " tile: (null)");
-                }*/
-                //Debug.Log("x:" + x + " y:" + y);
-                if (dangerTile[x + 5, y + 7] == 1)
-                {
-                    tilemap.SetTileFlags(new Vector3Int(x, y, 0), TileFlags.None);
-                    tilemap.SetColor(new Vector3Int(x, y, 0), Color.green);
-                }
-                //tilemap.SetTileFlags(new Vector3Int(x, y, 0), TileFlags.None);
-                //tilemap.SetColor(new Vector3Int(x, y, 0), Color.green);
-
+                //TileBase tile = allTiles[x + y * bounds.size.x];
+                tilemap.SetTileFlags(new Vector3Int(x, y, 0), TileFlags.None);
+                /*tilemap.SetColor(new Vector3Int(x, y, 0), Color.green);*/
+          
             }
         }
+
+    }
+
+    void processArrayColor()
+    {
+        /* for (int x = bounds.xMin + 4; x < bounds.xMax - 1; x++)
+         {
+             for (int y = bounds.yMin; y < bounds.yMax - 4; y++)
+             {
+                 *//*TileBase tile = allTiles[x + y * bounds.size.x];
+                 if (tile != null)
+                 {
+                     Debug.Log("x:" + x + " y:" + y + " tile:" + tile.name);
+                 }
+                 else
+                 {
+                     Debug.Log("x:" + x + " y:" + y + " tile: (null)");
+                 }*//*
+                 //Debug.Log("x:" + x + " y:" + y);
+                 if (dangerTile[x + 5, y + 7] == 1)
+                 {
+                     tilemap.SetTileFlags(new Vector3Int(x, y, 0), TileFlags.None);
+                     tilemap.SetColor(new Vector3Int(x, y, 0), Color.green);
+                 }
+                 //tilemap.SetTileFlags(new Vector3Int(x, y, 0), TileFlags.None);
+                 //tilemap.SetColor(new Vector3Int(x, y, 0), Color.green);
+
+             }
+         }*/
+        objX = (int) Math.Floor(playerObj.transform.position.x);
+        objY = (int) Math.Floor(playerObj.transform.position.y);
+        dangerTile[objX + 5, objY + 7] += 1;
+
+        Color color = Color.green;
+        color.a = dangerTile[objX + 5, objY + 7] * 0.2f;
+        tilemap.SetColor(new Vector3Int(objX, objY, 0), color);
+        //Debug.Log(dangerTile[objX + 5, objY + 7]);
+
+
 
     }
     // Update is called once per frame
     void Update()
     {
-        
+        //Debug.Log("Player Position: X = " + Math.Floor(playerObj.transform.position.x) + " --- Y = " + Math.Floor(playerObj.transform.position.y));
     }
 }
