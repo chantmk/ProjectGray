317f72812d362de8a84afae372a4ecf192e1bc29
Krit Cholapand <flap.atyz@gmail.com>
2021-03-23 12:40:50 +0700

    ✨ Add StateMachine and example

---
+++ Assets/Scripts/StateMachine/StateMachine.cs
@@ -0,0 +1,56 @@
+﻿using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+using TMPro.EditorUtilities;
+using UnityEngine;
+
+public class StateMachine
+{
+    public int StateId { get; private set; }
+
+    private State state;
+    private int defaultStateId;
+    private Dictionary<int, State> states = new Dictionary<int, State>();
+    private bool isJustInit;
+
+    public void Initialize(int startId, int defaultId)
+    {
+        isJustInit = true;
+        SetState(startId);
+        state = states[startId];
+        defaultStateId = defaultId;
+    }
+
+    public void Process()
+    {
+        var prevState = state;
+        if (prevState.Id != StateId)
+        {
+            prevState.EndCb?.Invoke();
+
+            state = states[StateId];
+
+            state.BeginCb?.Invoke();
+        } 
+        else if (isJustInit)
+        {
+            if (!(prevState.BeginCb is null))
+                state.BeginCb();
+            isJustInit = false;
+        }
+
+        SetState(state.UpdateCb?.Invoke() ?? defaultStateId); // on update return next state
+    }
+
+    public void AddState(State state)
+    {
+        states[state.Id] = state;
+    }
+
+    public void SetState(int stateId)
+    {
+        StateId = stateId;
+    }
+}
---
+++ Assets/Scripts/StateMachine/StateMachine_example.cs
@@ -0,0 +1,167 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+/// <summary>
+// Disclaimer !!!!!!!!!!!!!!!!!!!!!!!
+// All naming convention is not for this project
+// Please DO NOT reference naming convention from this example
+/// </summary>
+public class PlayerController : MonoBehaviour
+{
+    public PlayerMovement PlayerMovement;
+    public float InvulnerableDuration;
+    public int MaxJuice = 3;
+    public int Juice;
+    public int Life = 5;
+    public float boostDistance;
+    public float FreeCostStateDuration;
+    public float SuperStateDuration;
+    public int DashJuice;
+    public int JumpJuice;
+
+    private bool _isFreeJump;
+
+    public bool IsInvulnerable;
+
+    public bool IsImmortal;
+
+    private Text _stateText;
+    // states enum
+    private const int NORMAL = 0;
+    private const int SUPER = 1;
+    private const int FREE_COST = 2;
+    private StateMachine stateMachine;
+
+    [SerializeField] private GameObject _barrier;
+    [SerializeField] private GameObject _hurtBox;
+
+    private float superStateTimer;
+    private float freeCostStateTimer;
+
+    private void Awake()
+    {
+        _stateText = GetComponentInChildren<Text>();
+
+        Juice = MaxJuice;
+    }
+    void Start()
+    {
+        // Add possible state
+        stateMachine = new StateMachine();
+        stateMachine.AddState(new State(NORMAL, null, null, null));
+        stateMachine.AddState(new State(SUPER, SuperBegin, SuperUpdate, SuperEnd));
+        stateMachine.AddState(new State(FREE_COST, FreeCostBegin, FreeCostUpdate, null));
+        stateMachine.Initialize(NORMAL, NORMAL);
+        //_gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+    }
+
+    private void SuperBegin()
+    {
+        superStateTimer = SuperStateDuration;
+        SetImmortal(true);
+    }
+    private void SuperEnd()
+    {
+        SetImmortal(false);
+    }
+
+    private int SuperUpdate()
+    {
+        superStateTimer -= Time.fixedDeltaTime;
+        if (superStateTimer <= 0)
+        {
+            return NORMAL;
+        }
+        return SUPER;
+    }
+
+    private void FreeCostBegin()
+    {
+        freeCostStateTimer = FreeCostStateDuration;
+    }
+
+    private int FreeCostUpdate()
+    {
+        RefillJuice();
+        freeCostStateTimer -= Time.fixedDeltaTime;
+        if (freeCostStateTimer <= 0)
+        {
+            return NORMAL;
+        }
+        return FREE_COST;
+    }
+
+    private void SetImmortal(bool value)
+    {
+        if (value)
+        {
+            IsImmortal = true;
+
+            _barrier.SetActive(true);
+            _hurtBox.SetActive(false);
+            return;
+        }
+        IsImmortal = false;
+        _barrier.SetActive(false);
+        _hurtBox.SetActive(true);
+    }
+
+    public void TakeDamage()
+    {
+        GetComponent<SpriteRenderer>().color = new Color(0, 0, 1);
+        IsInvulnerable = true;
+        Invoke("SetInvulnerableFalse", InvulnerableDuration);
+        Life -= 1;
+        if (Life <= 0)
+        {
+            SceneManager.LoadScene("GameOver");
+        }
+    }
+
+    public void RefillJuice()
+    {
+        Juice = MaxJuice;
+    }
+
+    public void StartSuper()
+    {
+        stateMachine.SetState(SUPER);
+    }    
+
+    public void StartBoost()
+    {
+        SetImmortal(true);
+        PlayerMovement.Boost(boostDistance);
+    }
+
+    public void StartFreeCost()
+    {
+        stateMachine.SetState(FREE_COST);
+    }
+
+    void SetInvulnerableFalse()
+    {
+        GetComponent<SpriteRenderer>().color = new Color(1, 1, 1);
+        IsInvulnerable = false;
+    }
+
+    void Update()
+    {
+        // For Debug
+        if (stateMachine.StateId == NORMAL)
+            _stateText.text = "Normal";
+        else if (stateMachine.StateId == SUPER)
+            _stateText.text = "Super (Item3)";
+        else if (stateMachine.StateId == FREE_COST)
+            _stateText.text = "Free Cost (Item1)";
+        else
+            _stateText.text = "Unknown";
+    }
+
+    private void FixedUpdate()
+    {
+        stateMachine.Process();
+    }
+
+}
