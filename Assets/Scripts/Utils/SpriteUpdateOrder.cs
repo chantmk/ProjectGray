fbb570f565a5af1d3cbb51dc0f29622fbee1b6f0
Krit Cholapand <flap.atyz@gmail.com>
2021-04-02 00:09:03 +0700

    ✨ Add sprite order update script

---
+++ Assets/Scripts/Utils/SpriteUpdateOrder.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteUpdateOrder: MonoBehaviour
+{
+    private SpriteRenderer spriteRenderer;
+ 
+    private void Start () {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+ 
+    private void Update () { 
+        spriteRenderer.sortingOrder = -Mathf.RoundToInt(transform.position.y * 100);
+    }
+    
+}
