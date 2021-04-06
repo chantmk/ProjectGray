8a121ef3d110ebbefac071b68279bee77988b57b
Kueakun <kueakun.liwc@gmail.com>
2021-04-06 20:01:19 +0700

    :sparkles: Add Character stats script

---
+++ Assets/Scripts/Stats/CharacterStats.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterStats : MonoBehaviour
+{
+    [SerializeField] public float maxHealth;
+    [SerializeField] public float currentHealth { get; private set; }
+
+    public Stats damage = 0.0f;
+    public Stats armor = 0.0f;
+    
+    void Awake()
+    {
+        currentHealth = maxHealth;
+    }
+
+    public void TakeDamage(float damage)
+    {
+        damage -= this.armor.GetValue();
+
+        damage = Mathf.Clamp(damage, 0, float.MaxValue);
+
+        currentHealth -= damage;
+        Debug.Log(transform.name + " -" + damage + " Health");
+        if (currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+    public virtual void Die()
+    {
+        Debug.Log(transform.name + " Died")
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            TakeDamage(5.0f);
+        }
+    }
+}
---
+++ Assets/Scripts/Stats/Stats.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Stats
+{
+    [SerializeField] private float baseValue;
+
+    private List<float> modifiers = new List<float>();
+
+    public int GetValue()
+    {
+        int finalValue = baseValue;
+
+        modifiers.ForEach(x => finalValue += x);
+        return finalValue;
+    }
+
+    public void AddModifier(int mod)
+    {
+        if (mod != 0) modifiers.Add(mod);
+    }
+
+    public void RemoveModifier(int mod)
+    {
+        if (mod != 0) modifiers.Remove(mod);
+    }
+}
