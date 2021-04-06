9f5ffda3f4fab77c8f0fcd95967e28370ba4d480
Kueakun <kueakun.liwc@gmail.com>
2021-04-07 00:21:07 +0700

    :sparkles: Add PlayerStats script and update CharacterStats

---
+++ Assets/Scripts/Stats/PlayerStats.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStats : CharacterStats
+{
+
+    protected override void Start()
+    {
+        base.Start();
+        
+    }
+    
+    //protected override void Update()
+    //{
+
+    //}
+
+}
