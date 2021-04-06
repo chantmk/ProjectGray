45895e9f0761d12e5a8221076c659e6cdf8d0712
ThammakornK <chan_tham@hotmail.com>
2021-04-07 02:10:31 +0700

    :beers: Bost last stand dialogue

---
+++ Assets/TempButton.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TempButton : MonoBehaviour
+{
+    public DialogueManager dm;
+    public Boss boss;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // TODO: remove (For debugging)
+        if (Input.GetKeyDown(KeyCode.N))
+        {
+            dm.DisplayNextSentence();
+        }
+        else if (Input.GetKeyDown(KeyCode.V))
+        {
+            boss.ChooseMercy();
+        }
+        else if (Input.GetKeyDown(KeyCode.B))
+        {
+            boss.ChooseKill();
+        }
+    }
+}
