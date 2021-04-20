bf9363a2ba42b9a9bc8d3661c84fb310b518cb39
ThammakornK <chan_tham@hotmail.com>
2021-04-20 22:58:34 +0700

    :construction: Yellow Boss skill

---
+++ Assets/Scripts/Projectile/Balloon.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Balloon : Projectile
+{
+    protected override void Attack(GameObject target)
+    {
+
+    }
+}
