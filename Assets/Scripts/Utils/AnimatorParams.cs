7cee72540cb257a57fdffa757ade4e3ef4b9175f
Krit Cholapand <flap.atyz@gmail.com>
2021-04-22 06:24:08 +0700

    ✨ Add Math and Animation Utils

---
+++ Assets/Scripts/Utils/AnimatorParams.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace Utils
+{
+    public static class AnimatorParams
+    {
+        public static int Horizontal = Animator.StringToHash("Horizontal");
+        public static int Vertical = Animator.StringToHash("Vertical");
+        public static int State = Animator.StringToHash("State");
+    }
+}
