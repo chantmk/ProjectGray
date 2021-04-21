07a04f45ef80482d7c311a40634964a56cd7e747
Kueakun <kueakun.liwc@gmail.com>
2021-04-21 22:59:06 +0700

    :construction: WIP UI transition

---
+++ Assets/ResemblanceRedeemManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResemblanceRedeemManager : Interactable
+{
+    private int[] checkpointAmount = { 2, 4, 6 };
+    public GameObject player;
+
+    // Start is called before the first frame update
+    protected override void Start()
+    {
+        base.Start();
+        player = GameObject.FindGameObjectsWithTag("Player")[0];
+    }
+
+    // Update is called once per frame
+    protected override void Update()
+    {
+        base.Update();
+    }
+
+    public override void OnInteract()
+    {
+        //show ui
+        Debug.Log("Interact with resemblance redemption");
+        //render ui according to the amount of resemblance player has
+
+        //render redeem button
+
+    }
+}
