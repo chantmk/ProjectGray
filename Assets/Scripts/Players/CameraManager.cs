e17547218b8a01ee64dbae1db7fbe9d794493b93
Krit Cholapand <flap.atyz@gmail.com>
2021-05-01 18:32:35 +0700

    :hammer: Set camera to template scene

--- Assets/Scripts/Players/CameraManager.cs
+++ Assets/Scripts/Players/CameraManager.cs
@@ -16,7 +16,7 @@ namespace Players
         void Update()
         {
             // Define a target position above and behind the target transform
-            Vector3 targetPosition = target.TransformPoint(new Vector3(0, 5, -10));
+            Vector3 targetPosition = target.TransformPoint(new Vector3(0, 0, -10));
 
             // Smoothly move the camera towards that target position
             transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, SmoothTime);
