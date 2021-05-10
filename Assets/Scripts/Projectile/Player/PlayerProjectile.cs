36e4d00d8d018559c79a728a5a19b1d6f53d163a
Krit Cholapand <flap.atyz@gmail.com>
2021-05-10 22:45:34 +0700

    :construction: Black bullet

---
+++ Assets/Scripts/Projectile/Player/PlayerProjectile.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerProjectile : Projectile
+{
+
+    protected override List<int> targetLayers
+    {
+        get { return _targetLayers; }
+    }
+    private List<int> _targetLayers;
+
+    private void Awake()
+    {
+        _targetLayers = new List<int>() {LayerMask.NameToLayer("Enemy"), LayerMask.NameToLayer("Boss")};
+    }
+
+    protected override void Attack(GameObject target)
+    {
+        target.GetComponent<EnemyStats>().TakeDamage(damage);
+    }
+}
+
+// public class Player : PlayereProjectile
+// {
+//     
+// }
