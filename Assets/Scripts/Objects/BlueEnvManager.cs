724ad8ea8e9cee9fdeb5fee73a781d3576211405
Krit Cholapand <flap.atyz@gmail.com>
2021-05-11 22:27:59 +0700

    :sparkles: Add env manager script

---
+++ Assets/Scripts/Objects/BlueEnvManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using Objects;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+// [ExecuteAlways]
+public class BlueEnvManager : MonoBehaviour
+{
+    [SerializeField] private GameObject puddlePrefabs;
+    private Dictionary<Tuple<int, int>, BlueEnvPuddleManager> puddleDict;
+
+    public TileBase tileA;
+    // Start is called before the first frame update 
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
