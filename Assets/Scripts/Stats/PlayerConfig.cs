9138d425b5067976c274b446ff97aeadb6c6d66e
Krit Cholapand <flap.atyz@gmail.com>
2021-06-11 05:49:53 +0700

    Change scene and level

--- Assets/Scripts/Stats/PlayerConfig.cs
+++ Assets/Scripts/Stats/PlayerConfig.cs
@@ -8,7 +8,7 @@ public static class PlayerConfig
     public static int HealthPackCount = 0;
     public static int ResemblanceCount = 0;
     public static int healValue = 5;
-    public static List<int> HaveWeapon = new List<int>{(int)WeaponIDEnum.Cheap, (int)WeaponIDEnum.Blue};
+    public static List<int> HaveWeapon = new List<int>{(int)WeaponIDEnum.Cheap};
     public static bool IsWeaponBlueSpecial;
     public static bool IsWeaponBlackSpecial;
 
