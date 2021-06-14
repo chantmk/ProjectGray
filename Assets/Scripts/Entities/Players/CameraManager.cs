8c8be16870231b81e0475b12cd373c9a793362d0
Krit Cholapand <flap.atyz@gmail.com>
2021-06-14 09:01:47 +0700

    Fix camera

--- Assets/Scripts/Entities/Players/CameraManager.cs
+++ Assets/Scripts/Entities/Players/CameraManager.cs
@@ -18,6 +18,10 @@ namespace Players
         {
             // Define a target position above and behind the target transform
             Vector3 targetPosition = target.TransformPoint(new Vector3(0, 0, -10));
+            if (target.position.x > 13)
+            {
+                targetPosition = new Vector3(12, 4.3f, -10);
+            }
 
             // Smoothly move the camera towards that target position
             CameraHolder.transform.position = Vector3.SmoothDamp(CameraHolder.transform.position, targetPosition, ref velocity, SmoothTime);
