8c28b61852dcdf9f7dda3d8f61d090c3aec3ccd0
Krit Cholapand <flap.atyz@gmail.com>
2021-06-14 08:25:19 +0700

    Mind guardian better

--- Assets/Scripts/PlayerHUD/MindManager.cs
+++ Assets/Scripts/PlayerHUD/MindManager.cs
@@ -10,13 +10,19 @@ public class MindManager : MonoBehaviour
     private GameObject blackMind;
     private GameObject blackGuardian;
     private Image blackBar;
+    private Image blackWing;
     private bool isBlackGuard = false;
 
     private GameObject blueMind;
     private GameObject blueGuardian;
     private Image blueBar;
+    private Image blueWing;
     private bool isBlueGuard = false;
     private Animator mindAnim;
+    private Color blackOgCol;
+    private Color blueOgCol;
+    private Color blackBarOgCol;
+    private Color blueBarOgCol;
 
     // Start is called before the first frame update
     void Start()
@@ -24,12 +30,17 @@ public class MindManager : MonoBehaviour
         blackMind = transform.Find("BlackMindContainer").gameObject;
         blackGuardian = blackMind.transform.Find("Guardian").gameObject;
         blackBar = blackMind.transform.Find("BlackBar").GetComponent<Image>();
+        blackWing = blackMind.transform.Find("Icon").GetComponent<Image>();
         blackBar.fillAmount = 0.0f;
+        blackOgCol = blackWing.canvasRenderer.GetColor();
 
         blueMind = transform.Find("BlueMindContainer").gameObject;
         blueGuardian = blueMind.transform.Find("Guardian").gameObject;
         blueBar = blueMind.transform.Find("BlueBar").GetComponent<Image>();
+        blueWing = blueMind.transform.Find("Icon").GetComponent<Image>();
         blueBar.fillAmount = 0.0f;
+        blueOgCol = blueWing.canvasRenderer.GetColor();
+        
         mindAnim = GetComponent<Animator>();
         EventPublisher.SetGuardianUI += SetGuardian;
     }
@@ -42,8 +53,38 @@ public class MindManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        
         blackGuardian.SetActive(isBlackGuard);
         blueGuardian.SetActive(isBlueGuard);
+
+        if (PlayerConfig.HaveResemblanceDict[ColorEnum.Blue] != blueWing.enabled)
+        {
+            blueWing.enabled = PlayerConfig.HaveResemblanceDict[ColorEnum.Blue];
+        }
+
+        if (PlayerConfig.HaveResemblanceDict[ColorEnum.Black] != blackWing.enabled)
+        {
+            blackWing.enabled = PlayerConfig.HaveResemblanceDict[ColorEnum.Black];
+        }
+
+        if (PlayerConfig.HaveResemblanceDict[ColorEnum.Blue] && blueWing.enabled)
+        {
+            if (isBlueGuard)
+                blueWing.color = blueOgCol;
+            else
+                blueWing.color = Color.black;
+        }
+        
+        if (PlayerConfig.HaveResemblanceDict[ColorEnum.Black] && blackWing.enabled)
+        {
+            if (isBlackGuard)
+                blackWing.color = blackOgCol;
+            else
+                blackWing.color = Color.black;
+        }
+        
+        
+        
     }
 
     public void UpdateBar(ColorEnum colorEnum, float ratio)
